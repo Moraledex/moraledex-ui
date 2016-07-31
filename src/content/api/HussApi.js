f835c1609879ad58979086fe5249b1cbb2ba1ff7
Adam Lock <adam.lock@workday.com>
2016-07-31 06:02:36 -0500

    Quick fix

--- src/content/api/HussApi.js
+++ src/content/api/HussApi.js
@@ -3,6 +3,9 @@ const realData = [];
 const HussApi = {
 
   getData: function() {
+    var sentiment;
+    var sentSum;
+    var sentiments = [];
     $.ajax({
       url: "https://k6xiqf7156.execute-api.us-east-1.amazonaws.com/prod/getSentiments",
       headers: { "x-api-key": "B4ncJJtsizapj44wrGbgO8k6lfApO0FIfzEicLZ6" },
@@ -31,6 +34,7 @@ const HussApi = {
         if(googData.status != "ZERO_RESULTS"){
           lng = googData.results[0].geometry.location.lng;
           lat = googData.results[0].geometry.location.lat;
+          console.log(`lat: ${lat}, lng: ${lng}, sentiment: ${sentiment}`);
           realData.push({
             lat: lat,
             lng: lng,
